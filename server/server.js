bd9469d1e3b5698805007f56d5e7b98a7f10c44d
Prakhar Ranjan Srivastava <prakharranjan12@gmail.com>
2026-04-06 00:52:13 +0530

    Added Vercel URL to CORS policy

--- server/server.js
+++ server/server.js
@@ -11,7 +11,13 @@ const app = express()
 await connectCloudinary();
 
 
-app.use(cors())
+app.use(cors({
+    origin: [
+        'http://localhost:5173', 
+        'https://all-ai-lilac.vercel.app'
+    ],
+    credentials: true
+}));
 app.use(express.json())
 app.use(clerkMiddleware())
 
